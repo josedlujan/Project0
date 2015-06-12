40059b0de6be65a70d17a6d7f4283cdf2372f734
josedlujan <josedlujan@gmail.com>
2015-06-12 00:15:44 -0500

    new commit

--- MyAppPortfolio/app/src/main/java/com/portfolio/app/my/josedlujan/nanodegree/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/portfolio/app/my/josedlujan/nanodegree/myappportfolio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.portfolio.app.my.josedlujan.nanodegree.myappportfolio;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -58,7 +59,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+    /*    switch (v.getId()){
             case R.id.spotify:
                 Toast.makeText(MainActivity.this,"This button will launch my Spotify Streamer App!",Toast.LENGTH_SHORT).show();
                 break;
@@ -78,6 +79,24 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                 Toast.makeText(MainActivity.this,"This button will launch my Capstone App!",Toast.LENGTH_SHORT).show();
                 break;
 
-        }
+        }*/
+        displayToast(v);
+
+    }
+
+    public void displayToast(View view) {
+
+        Button button = (Button) view;
+
+        String buttonText = (String) button.getText();
+
+        Context context = getApplicationContext();
+
+        CharSequence text = getString(R.string.open_app)
+                + buttonText;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
